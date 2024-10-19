8c48a64420fe03c06faa2b376d38e7912bca9153
hirnlego <roberto@noris.me>
2024-10-19 09:22:02 +0200

    Lowered looper buffer size again

--- Commons.h
+++ Commons.h
@@ -71,8 +71,7 @@ constexpr float kOscFreqMax = 8219.f; // C9
 
 constexpr int kLooperLoopLengthMin = 367; // Almost C3 (48000 / 130.81f)
 constexpr int kLooperFadeSamples = 480; // 10ms @ audio rate
-static const int32_t kLooperTotalBufferLength = 672000; // Samples for both channels (interleaved) = 7 seconds stereo buffer
-//static const int32_t kLooperTotalBufferLength = 1 << 19; // 524288 samples for both channels (interleaved) = 5.46 seconds stereo buffer
+static const int32_t kLooperTotalBufferLength = 1 << 19; // 524288 samples for both channels (interleaved) = 5.46 seconds stereo buffer
 static const int32_t kLooperChannelBufferLength = kLooperTotalBufferLength / 2;
 static const float kLooperFadeInc = 1.f / kLooperFadeSamples;
 constexpr float kLooperMakeupGain = 1.f;
